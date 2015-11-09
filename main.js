34a10c6edf2a8143283197e710da4630258edc1b
oeuillot <olivier.oeuillot@gmail.com>
2015-11-09 17:50:42 +0100

    Add scenario

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ module.exports = {
   Syncer : Syncer
 };
 
+program.option("--scenario <path>", "Scenario file");
+
 program.option("-s, --source <path>", "Source directory");
 program.option("-d, --destination <path>", "Destination directory");
 program.option("--hubic-log", "Enable hubic log");
@@ -38,17 +40,44 @@ program.option("--clientSecret <secret>",
     "Specify the Hubic application Client Secret");
 program.parse(process.argv);
 
-function goHubic() {
+function goHubic(hubic, source, destination, callback) {
 
-  if (!program.source) {
-    console.log("Source is not specified");
-    process.exit(1);
+  var syncer = new Syncer(hubic, program);
+
+  if (destination.charAt(0) === '/') {
+    destination = destination.substring(1);
   }
 
-  if (!program.destination) {
-    console.log("Destination is not specified");
-    process.exit(1);
-  }
+  var hroot = HFile.createRoot(hubic);
+  var lroot = LFile.createRoot(source);
+  hroot.find(destination, function(error, hfile) {
+    if (error) {
+      console.error("Can not find destination: '" + destination + "' ", error);
+
+      return callback(error);
+    }
+
+    if (!lroot) {
+      return callback(new Error("Invalid local root for source " +
+          program.source));
+    }
+    if (!hfile) {
+      return callback(new Error("Invalid remote root for destination " +
+          destination));
+    }
+
+    syncer.sync(lroot, hfile, function(error) {
+      if (error) {
+        return callback(error);
+      }
+
+      callback();
+    });
+  });
+}
+
+if (program.scenario) {
+  var scenario = require(program.scenario);
 
   var hubic = new Hubic(program, function(error, hubic) {
     if (error) {
@@ -56,44 +85,60 @@ function goHubic() {
       return;
     }
 
-    var syncer = new Syncer(hubic, program);
+    async.eachSeries(scenario, function(sc, callback) {
+      var source = sc.source;
+      var destination = sc.destination;
 
-    var destination = program.destination;
-    if (destination.charAt(0) === '/') {
-      destination = destination.substring(1);
-    }
-
-    var hroot = HFile.createRoot(hubic);
-    var lroot = LFile.createRoot(program.source);
-    hroot.find(destination, function(error, hfile) {
-      if (error) {
-        console
-            .error("Can not find destination: '" + destination + "' ", error);
-        return;
+      if (!source || !destination) {
+        console.error("Source or destination are not defined !");
+        return callback("Source or destination are not defined !");
       }
 
-      if (!lroot) {
-        throw new Error("Invalid local root for source " + program.source);
-      }
-      if (!hfile) {
-        throw new Error("Invalid remote root for destination " + destination);
-      }
-
-      syncer.sync(lroot, hfile, function(error) {
+      goHubic(hubic, source, destination, function(error) {
         if (error) {
           console.error("Can not sync: ", error);
           return;
         }
 
-        console.log("Waiting last uploads ...");
+        callback();
+      });
+    }, function(error) {
+      console.log("Waiting last uploads ...");
 
-        hubic.flush(function() {
-          console.log("Done !");
-        });
+      hubic.flush(function() {
+        console.log("Done !");
       });
     });
   });
-
+  return;
 }
 
-goHubic();
+if (!program.source) {
+  console.log("Source is not specified");
+  process.exit(1);
+}
+
+if (!program.destination) {
+  console.log("Destination is not specified");
+  process.exit(1);
+}
+
+var hubic = new Hubic(program, function(error, hubic) {
+  if (error) {
+    console.error("Can not create hubic context: ", error);
+    return;
+  }
+
+  goHubic(hubic, program.source, program.destination, function(error) {
+    if (error) {
+      console.error("Can not sync: ", error);
+      return;
+    }
+
+    console.log("Waiting last uploads ...");
+
+    hubic.flush(function() {
+      console.log("Done !");
+    });
+  });
+});
